8a8bd0c4af6a104a94287417b5e9b3bb0c32e358
Rjarticle <139971262+Rjarticle@users.noreply.github.com>
2024-07-29 16:27:31 +0530

    Add files via upload

---
+++ ContactUs.php
@@ -0,0 +1,132 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="cs" />
+    <meta name="robots" content="all,follow" />
+
+    
+    <title>ONLINE JOB-PORTAL </title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    
+    <link rel="index" href="./" title="Home" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="./css/main.css" />
+    <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+    <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+    <style type="text/css">
+<!--
+.style1 {
+	color: #000066;
+	font-weight: bold;
+}
+-->
+    </style>
+    <script src="SpryAssets/SpryValidationTextarea.js" type="text/javascript"></script>
+    <link href="SpryAssets/SpryValidationTextarea.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body id="www-url-cz">
+<!-- Main -->
+<div id="main" class="box">
+<?php 
+include "Header.php"
+?>
+<?php 
+include "menu.php"
+?>   
+<!-- Page (2 columns) -->
+    <div id="page" class="box">
+    <div id="page-in" class="box">
+
+        <div id="strip" class="box noprint">
+
+            <!-- RSS feeds -->
+            <hr class="noscreen" />
+
+            <!-- Breadcrumbs -->
+            <p id="breadcrumbs">&nbsp;</p>
+          <hr class="noscreen" />
+            
+        </div> <!-- /strip -->
+
+        <!-- Content -->
+        <div id="content">
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+            
+            <!-- Article -->
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+            <!-- Article -->
+            <div class="article">
+                <h2><span><a href="#">Contact Us</a></span></h2>
+               
+
+                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                  <tr>
+                    <td width="6%"><img src="design/ico_archive2.gif" alt="" width="9" height="11" /></td>
+                    <td width="94%"><strong>jobportal.com<br/>
+                    MG road,<br/>
+                    Mumbai, India<br/>
+                    Pincode:425201</strong></td>
+                  </tr>
+                  <tr>
+                    <td><img src="design/ico_archive2.gif" alt="" width="9" height="11" /></td>
+					<td><strong>Office No:425201<b>
+					Mobile: +91-9822866172
+					</strong></td>
+					
+                  </tr>
+                  <tr>
+                    <td><img src="design/ico_archive2.gif" alt="" width="9" height="11" /></td>
+                    <td><strong>Email:jobportal.com</strong></td>
+                  </tr>
+                  <tr>
+                    <td><img src="design/ico_archive2.gif" alt="" width="9" height="11" /></td>
+                    <td><strong>Website:jobportal.com</strong></td>
+                  </tr>
+			
+                </table>
+				<p><img src="https://imgs.search.brave.com/0jdyuauJI9_hmhQSBNBXB5tcou-HwJvW2zVNDg8D2uk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE2/MzUzNTA3MzY0NzUt/YzhjZWY0YjIxOTA2/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4TVRGOGZH/cHZZbnhsYm53d2ZI/d3dmSHg4TUE9PQ" alt="" width="500" height="300" /></p>
+              
+              <p>&nbsp;</p>
+
+              <p class="btn-more box noprint">&nbsp;</p>
+          </div> <!-- /article -->
+
+            <hr class="noscreen" />
+            
+        </div> <!-- /content -->
+
+        <div>
+
+
+<?php
+include "right.php"
+?>
+
+    </div> <!-- /page-in -->
+    </div> <!-- /page -->
+
+ 
+<?php
+include "footer.php"
+?>
+</div> <!-- /main -->
+
+
+</body>
+</html>
